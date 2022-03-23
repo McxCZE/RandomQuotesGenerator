e1a766a8d11b5247ff22f34d34f73488cf5d70f7
Mcx <milan.cihak@brouzdej.net>
2022-03-23 20:02:34 +0100

    whoknows

--- sources/webFunctions.js
+++ sources/webFunctions.js
@@ -1,27 +1,27 @@
-//#region - GenerateFuntions
+//helperFn
+function generateRandomInteger(max) {
+    return Math.floor(Math.random() * max);
+};
 
-//#region - Hlavní stránka
+//mainFn
 $.fn.generateMain = function () {
-    // $('#mainApp').load("subPages/main.html", function (responseTxt, statusTxt, jqXHR) {
-    //     if (statusTxt == "success") {
-            $.getJSON("sources/quotesCollection.json", function (jsonResponse) {
-                    var _sayingHtml = "";
-                    var _jsonResponseQuote = jsonResponse.Quote;
-                    var _jsonResponsePerson = jsonResponse.Person;
-                    _sayingHtml += `<h1>${_jsonResponseQuote[0].saying[0]}.</h1>`;
-                    _sayingHtml += `<h3>${_jsonResponsePerson[0].person[0]}.</h3>`;
-                    $('#dynamicDiv').html(_sayingHtml);
-                })
-                .done(function () {
-                    console.log("done");
-                })
-                .fail(function () {
-                    console.log("error");
-                })
-                .always(function () {
-                    console.log("complete");
-                });
-        // };
-        // if (statusTxt == "error") { alert("Error: " + jqXHR.status + " " + jqXHR.statusText) };
-    // });
-}
+    $.getJSON("sources/quotesCollection.json", function (jsonResponse) {
+        var sayingHtml = "";
+
+        var jsonResponseQuote = jsonResponse.Quote;
+        var jsonResponsePerson = jsonResponse.Person;
+
+        var indexQuote = generateRandomInteger(jsonResponseQuote[0].quotes.length);
+        var indexPerson = generateRandomInteger(jsonResponsePerson[0].people.length);
+
+        sayingHtml += `<div class='quote'>${jsonResponseQuote[0].quotes[indexQuote]}.</div>`;
+        sayingHtml += `<div class='person'>${jsonResponsePerson[0].people[indexPerson]}.</div>`;
+        $('#dynamicDiv').html(sayingHtml);
+    })
+        .done(function () {
+            console.log("generováno");
+        })
+        .fail(function () {
+            console.log("fpiči");
+        });
+};
