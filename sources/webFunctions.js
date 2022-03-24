05c8e9c085b9d53c6a36af021f638a4ec2f2a324
Mcx <milan.cihak@brouzdej.net>
2022-03-24 08:34:41 +0100

    Update - Styling

--- sources/webFunctions.js
+++ sources/webFunctions.js
@@ -14,8 +14,8 @@ $.fn.generateMain = function () {
         var indexQuote = generateRandomInteger(jsonResponseQuote[0].quotes.length);
         var indexPerson = generateRandomInteger(jsonResponsePerson[0].people.length);
 
-        sayingHtml += `<div class='quote'>${jsonResponseQuote[0].quotes[indexQuote]}.</div>`;
-        sayingHtml += `<div class='person'>${jsonResponsePerson[0].people[indexPerson]}.</div>`;
+        sayingHtml += `<div class='quote'>${jsonResponseQuote[0].quotes[indexQuote]}</div>`;
+        sayingHtml += `<div class='person'>${jsonResponsePerson[0].people[indexPerson]}</div>`;
         $('#dynamicDiv').html(sayingHtml);
     })
         .done(function () {
